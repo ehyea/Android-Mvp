5f656464f7b97d1638f2b5251f8bd259c05da009
Eric <ehyea@163.com>
2018-10-23 11:18:49 +0800

    安卓mvp框架
    安卓mvp框架

---
+++ mvplib/src/main/java/cn/eric/mvplib/retrofit/okhttp/HttpCache.java
@@ -0,0 +1,20 @@
+package cn.eric.mvplib.retrofit.okhttp;
+import cn.eric.mvplib.utils.AppUtils;
+import okhttp3.Cache;
+
+import java.io.File;
+
+/**
+ * Created by Horrarndoo on 2017/9/12.
+ * <p>
+ */
+public class HttpCache {
+
+    private static final int HTTP_RESPONSE_DISK_CACHE_MAX_SIZE = 50 * 1024 * 1024;
+
+    public static Cache getCache() {
+        return new Cache(new File(AppUtils.getContext().getCacheDir().getAbsolutePath() + File
+                .separator + "data/NetCache"),
+                HTTP_RESPONSE_DISK_CACHE_MAX_SIZE);
+    }
+}
